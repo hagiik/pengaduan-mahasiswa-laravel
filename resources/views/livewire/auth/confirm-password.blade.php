8c3a8953b05a1baf563617ac59447e2e2c7b682f
hagiik <hagi1hsan3@gmail.com>
2025-04-18 23:00:02 +0700

    ✨ feat: Update redirect routes to use 'pengaduan.index' for email verification and password confirmation

--- resources/views/livewire/auth/confirm-password.blade.php
+++ resources/views/livewire/auth/confirm-password.blade.php
@@ -28,7 +28,7 @@ new #[Layout('components.layouts.auth')] class extends Component {
 
         session(['auth.password_confirmed_at' => time()]);
 
-        $this->redirectIntended(default: route('pengaduan', absolute: false), navigate: true);
+        $this->redirectIntended(default: route('pengaduan.index', absolute: false), navigate: true);
     }
 }; ?>
 
